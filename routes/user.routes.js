02dc9305f928c7c632db360f51adfd60897df570
Albert <albert.tardis05@gmail.com>
2023-01-10 11:32:18 +0100

    Create and implement get and post user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,36 @@
 import { Router } from "express";
+import { check } from "express-validator";
+
+import { getUser, createUser } from "../controllers/index.js";
+
+import { validateFields } from "../middlewares/index.js";
 
 const userRoutes = Router();
 
-const middlewares = {};
+const middlewares = {
+  getUser: [
+    check("username").notEmpty(),
+    // check("username").custom(),
+    validateFields,
+  ],
+  create: [
+    check("email", "Bad request - no email in body").notEmpty(),
+    check("email", "Bad request - not a valid email").isEmail(),
+    // check if the email exists in the database
+    check("name", "Bad request - no name in body").notEmpty(),
+    check("username", "Bad request - no username in body").notEmpty(),
+    // check if the username exists in the database
+    check(
+      "password",
+      "Bad request - password must have 6 or more characters"
+    ).isLength({
+      min: 6,
+    }),
+    validateFields,
+  ],
+};
 
-userRoutes.get("/", () => console.log("Get userRoutes"));
-userRoutes.post("/", () => console.log("Post userRoutes"));
+userRoutes.get("/:username", middlewares.getUser, getUser);
+userRoutes.post("/create", middlewares.create, createUser);
 
 export default userRoutes;
