b1a51ebb73fc2a436c079808825b217f94ca8104
Albert <albert.tardis05@gmail.com>
2023-01-11 12:43:50 +0100

    Add "code" in json response

--- middlewares/validateFields.js
+++ middlewares/validateFields.js
@@ -2,8 +2,9 @@ import { validationResult } from "express-validator";
 
 const validateFields = (req, res, next) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty())
-    return res.status(400).json({ error: true, data: errors });
+    return res.status(400).json({ code: 400, error: true, data: errors });
 
   next();
 };
