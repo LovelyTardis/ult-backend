1d0000a5fd531ea1c7b8120d8ddc37953d453a95
Albert <albert.tardis05@gmail.com>
2023-01-11 18:53:17 +0100

    Implement ult routes

--- routes/ult.routes.js
+++ routes/ult.routes.js
@@ -1,10 +1,25 @@
 import { Router } from "express";
+import { check } from "express-validator";
+
+// CONTROLLERS
+import { getUlt, createUlt } from "../controllers/index.js";
+
+// MIDDLEWARES
+import { validateFields } from "../middlewares/index.js";
 
 const ultRoutes = Router();
 
-const middlewares = {};
+const middlewares = {
+  getUlt: [validateFields],
+  create: [
+    check("user").isMongoId(),
+    check("message", "Message is required").notEmpty(),
+    // TODO: if ult in body, add the created ult as a comment for the other ult
+    validateFields,
+  ],
+};
 
-ultRoutes.get("/", () => console.log("Get ultRoutes"));
-ultRoutes.post("/", () => console.log("Post ultRoutes"));
+ultRoutes.get("/:ult", middlewares.getUlt, getUlt);
+ultRoutes.post("/", middlewares.create, createUlt);
 
 export default ultRoutes;
