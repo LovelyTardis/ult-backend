5a3a625db18736ce9a9b0fe0a473175cdb6664c6
Albert <albert.tardis05@gmail.com>
2023-01-19 13:20:52 +0100

    Update how errors are returned

--- middlewares/validators/validateFields.js
+++ middlewares/validators/validateFields.js
@@ -2,10 +2,11 @@ import { validationResult } from "express-validator";
 import { customError } from "../../utils/customError.js";
 
 export const validateFields = (req, res, next) => {
-  const errors = validationResult(req);
+  const result = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return next(customError(errors, 400));
+  if (!result.isEmpty()) {
+    const data = result.array().map((err) => err.msg);
+    return next(customError(data, 400));
   }
 
   next();
